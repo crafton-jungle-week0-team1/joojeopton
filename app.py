bebd64e21bff8f6ddb50681d431e61a4a3cd236a
bachohyeonho <70460688+joho54@users.noreply.github.com>
2025-03-10 23:00:37 +0900

    클라이언트 초안

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.contrib.github import make_github_blueprint, github
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
@@ -7,6 +7,15 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, curren
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # HTTP에서도 사용 가능하도록 설정
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    img_url = url_for('static', filename='images/profile_test.png')
+    return render_template("index.html", profile_url=img_url)
+@app.route('/joojeop')
+def joojeop():
+    img_url = url_for('static', filename='images/profile_test.png')
+    return render_template("joojeop.html", profile_url=img_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
